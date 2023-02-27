a036225f76d5944c38a51e7f73f14c676c07da51
Felipe-Aristizabal <andres.aristizabal_m@uao.edu.co>
2023-02-27 17:30:42 -0500

    Navbar Client realizada

---
+++ src/pages/client/HomeClient.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Layout, MainContentLayout, NavBarLayout } from "../../styled-components/index/Layout";
+import { NavBar, NavBarContainerLogo, NavBarContainerIMG, NavBarH1, NavBarContainerH, NavBarH2 } from "../../styled-components/index/Navbar";
+
+const Home = () => {
+	return (
+		<Layout>
+			<NavBarLayout>
+				<NavBar>
+					<NavBarContainerLogo>
+						<NavBarContainerIMG> a</NavBarContainerIMG>
+						<NavBarH1>NexusNet</NavBarH1>
+					</NavBarContainerLogo>
+					<NavBarContainerH>
+						<NavBarH2>Home</NavBarH2>
+						<NavBarH2>Top providers</NavBarH2>
+						<NavBarH2>Chats</NavBarH2>
+						<NavBarH2>Account</NavBarH2>
+						<NavBarH2>Log out</NavBarH2>
+					</NavBarContainerH>
+				</NavBar>
+			</NavBarLayout>
+		</Layout>
+	);
+};
+
+export default Home;
--- src/styled-components/index/Layout.js
+++ src/styled-components/index/Layout.js
@@ -2,21 +2,20 @@ import styled from "styled-components";
 
 const Layout = styled.div`
 	display: grid;
+	min-height: 100vh;
 	grid-template-areas: "navbar main";
-	grid-template-rows: 1fr;
-	grid-template-columns: 1fr 600px;
+	grid-template-rows: 100%;
+	grid-template-columns: 1.5fr 5fr;
 `;
 
 const NavBarLayout = styled.div`
-	font-size: 3rem;
-	background-color: #666666;
 	grid-area: navbar;
 `;
 
 const MainContentLayout = styled.div`
-	font-size: 2rem;
-	background-color: #efefef;
 	grid-area: main;
 `;
+
+
 
 export { Layout, MainContentLayout, NavBarLayout };
