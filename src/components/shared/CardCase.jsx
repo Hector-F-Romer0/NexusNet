ba144ed037b6acef8f2fe63ee8aae0a0e18613e5
Hector Romero <hector_fabio.romero@uao.edu.co>
2023-04-14 17:25:26 -0500

    Reconstrucción de página de inicio y diseño de la sidebar alterado usando Tailwind.

--- src/components/shared/CardCase.jsx
+++ src/components/shared/CardCase.jsx
@@ -1,31 +1,27 @@
 import React from "react";
-import {
-	CardCaseStyle,
-	CaseDate,
-	CaseDescription,
-	CaseSeparator,
-	CaseTitle,
-} from "../../styled-components/index/CardCase.style";
-import { TagContainerStyle } from "../../styled-components/index/Tags.style";
-
-import Tag from "./Tag";
 
 const CardCase = () => {
 	return (
-		<CardCaseStyle>
-			<TagContainerStyle>
-				<Tag title="Health" variant="category" />
-				<Tag title="Biomedical engineering" variant="service" />
-			</TagContainerStyle>
-			<CaseTitle>Title case</CaseTitle>
-			<CaseSeparator />
-			<CaseDescription>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis obcaecati tempora dicta, pariatur
-				sed temporibus aliquid corporis laboriosam eaque natus dolorum esse modi distinctio asperiores eius
-				voluptate illum deleniti possimus?
-			</CaseDescription>
-			<CaseDate>Taken on dd/mm/yy</CaseDate>
-		</CardCaseStyle>
+		<div className="block min-w-sm w-4/5 pt-5 px-10  rounded-lg shadow bg-card">
+			<div className="flex flex-row gap-5 mb-3 flex-wrap text-sm md:text-xl">
+				<div className="text-sm inline-flex items-center font-bold leading-sm px-7 py-1 bg-categoryTag rounded-full text-white">
+					Health
+				</div>
+				<div className=" text-sm inline-flex items-center font-bold leading-sm px-7 py-1 bg-serviceTag rounded-full text-white">
+					Services
+				</div>
+			</div>
+			<h3 className="mb-2 text-3xl font-bold tracking-tight text-black">Title case</h3>
+			<hr className="h-1 bg-black mb-5 f" />
+			<p className="font-light text-gray-700 text-sm md:text-xl">
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe repudiandae commodi facere voluptate
+				dolores aliquam, mollitia deserunt, quis illo dolorem velit a voluptatum nobis totam illum voluptatibus
+				non ipsum sequi. Esse nesciunt quo vero? Rem, soluta dolorum iusto eos distinctio repellat reiciendis
+				atque inventore voluptatibus vel sequi, ut nesciunt error esse accusamus accusantium! Perferendis
+				accusantium fugiat aperiam ipsum pariatur quibusdam.
+			</p>
+			<h4 className="font-thin text-base text-right text-gray-700 mt-4 pb-2">Taken on dd/mm/yy</h4>
+		</div>
 	);
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ export default {
 			colors: {
 				navbar: "#5A8FCC",
 				footer: "#2A4C73",
+				card: "#E8F1FF",
+				categoryTag: "#227FC2",
+				serviceTag: "#2B5E99",
+			},
+			fontFamily: {
+				montserrat: ["Montserrat", "sans-serif"],
 			},
 		},
 	},
