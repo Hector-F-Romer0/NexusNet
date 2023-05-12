827c9b5c7ac7e890542eb0651cada0cdea99a279
Felipe-Aristizabal <andres.aristizabal_m@uao.edu.co>
2023-05-12 18:59:06 -0500

    Metodo de sobreescritura de clave primaria para service y keyword

--- models/keyword.model.js
+++ models/keyword.model.js
@@ -4,6 +4,13 @@ export const keywordSchema = Schema({
 	label: { type: String, required: true },
 });
 
+//* Modificaremos el método del modelo que retorna el JSON del modelo. Esto lo haremos para arrojar en la respuesta del protocolo el "_id" de Mongo como "id"
+keywordSchema.methods.toJSON = function () {
+	const { __v, _id, ...category } = this.toObject();
+	category.id = _id;
+	return category;
+};
+
 const keyWordModel = model("Keyword", keywordSchema);
 
 export default keyWordModel;
--- models/service.model.js
+++ models/service.model.js
@@ -4,6 +4,13 @@ export const serviceSchema = Schema({
 	label: { type: String, required: true },
 });
 
+//* Modificaremos el método del modelo que retorna el JSON del modelo. Esto lo haremos para arrojar en la respuesta del protocolo el "_id" de Mongo como "id"
+serviceSchema.methods.toJSON = function () {
+	const { __v, _id, ...category } = this.toObject();
+	category.id = _id;
+	return category;
+};
+
 const serviceModel = model("Service", serviceSchema);
 
 export default serviceModel;
