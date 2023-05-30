ad6a36d1716134fc5b914ee0e3e38259964afffb
Hector Romero <hector_fabio.romero@uao.edu.co>
2023-05-30 12:07:32 -0500

    DEBUG: Analizar request de login

--- src/pages/index/SignIn.jsx
+++ src/pages/index/SignIn.jsx
@@ -40,6 +40,7 @@ const SignIn = () => {
 
 	const onSubmitSignIn = async (data) => {
 		const res = await loginUserRequest(data);
+		console.log(res);
 		if (res.status === 404) {
 			return await MySwal.fire({
 				title: "Incorrect data",
