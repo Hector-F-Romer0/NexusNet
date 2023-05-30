e05094faaa30f7e1d3c5265048cc4c5d6be829a7
Felipe-Aristizabal <andres.aristizabal_m@uao.edu.co>
2023-05-30 00:21:54 -0500

    Creación de componente Loading

---
+++ src/components/shared/Loading.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Loading = () => {
+	return (
+		<div class="h-screen bg-white">
+			<div class="flex justify-center items-center h-full">
+				<img
+					class="h-16 w-16"
+					src="https://tenor.com/es/view/spider-web-cobweb-catch-web-gif-14509485"
+					alt=""
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Loading;
